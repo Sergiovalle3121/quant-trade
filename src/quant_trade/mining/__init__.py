4df436a6130cf61935a95ec5087c81f43c8b7fa2
Sergiovalle3121 <sergiovallezarate@gmail.com>
2026-07-22 23:47:19 -0600

    Add cloud-aware mining profitability stack

---
+++ src/quant_trade/mining/__init__.py
@@ -0,0 +1,22 @@
+Exit code: 0
+Wall time: 1.2 seconds
+Output:
+"""Offline profitability and safety gates for authorized crypto mining."""
+
+from quant_trade.mining.models import (
+    MiningEvaluation,
+    MiningMarketSnapshot,
+    MiningPolicy,
+    MiningRig,
+)
+from quant_trade.mining.profitability import evaluate_all, evaluate_mining
+
+__all__ = [
+    "MiningEvaluation",
+    "MiningMarketSnapshot",
+    "MiningPolicy",
+    "MiningRig",
+    "evaluate_all",
+    "evaluate_mining",
+]
+
